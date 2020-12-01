3849ce5be53c1487ec6d120426a650482c0a8eb2
saoussen <sawsan_sli3i@live.fr>
2020-12-01 17:33:09 +0000

    refactoring

--- fetcher.js
+++ fetcher.js
@@ -6,9 +6,9 @@ const myArgs = process.argv.slice(2);
   request (myArgs[0], (error, response, body) => {
     fs.writeFile(myArgs[1],  body, (err) => {
       const downloadedFile = fs.statSync(myArgs[1]);
+      console.log(downloadedFile);
       const fileSize = downloadedFile["size"];
       console.log('Downloaded and saved ',fileSize, ' bytes to ', myArgs[1]);
-  
       });
   });
   
