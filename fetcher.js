bda5241bc06f04735a9cc4fbcf7a7d59d6a8bce6
saoussen <sawsan_sli3i@live.fr>
2020-12-01 17:40:21 +0000

    handled the error case

--- fetcher.js
+++ fetcher.js
@@ -5,6 +5,8 @@ const myArgs = process.argv.slice(2);
 
   request (myArgs[0], (error, response, body) => {
     fs.writeFile(myArgs[1],  body, (err) => {
+      if (error) { console.log('Error');
+                return;}
       const downloadedFile = fs.statSync(myArgs[1]);
       console.log(downloadedFile);
       const fileSize = downloadedFile["size"];
